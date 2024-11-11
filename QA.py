088b22b7b1cda6211123a2f28fae0c3fe65ac312
GJing <jingguo92@gmail.com>
2024-11-11 16:30:42 +0800

    Add QA module for generating questions and answers using LLM

---
+++ QA.py
@@ -0,0 +1,111 @@
+import json
+import logging
+import os
+import pickle
+
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+import pandas as pd
+from tools.data import merge_pickle_list, fix_utf8, trim_data
+
+load_dotenv()
+
+logging.basicConfig(
+    filename="QA.log",
+    level=logging.INFO,
+    format="%(asctime)s:%(levelname)s:%(message)s",
+    filemode="w",
+    force=True,
+)
+
+
+llm = ChatOpenAI(model="gpt-4o-mini")
+
+json_schema = {
+    "title": "Questions_and_Answers",
+    "description": "Generating questions and answers for evaluating LLM performance",
+    "type": "object",
+    "properties": {
+        "qa_pairs": {
+            "type": "array",
+            "items": {
+                "type": "object",
+                "properties": {
+                    "Question": {
+                        "type": "string",
+                        "description": "Question generated for evaluating LLM performance"
+                    },
+                    "Answer": {
+                        "type": "string",
+                        "description": "Answer to the question"
+                    },
+                    "Level": {
+                        "type": "string",
+                        "description": "Level of the question from perspective of environmenal domain",
+                        "enum": ["Easy", "Medium", "Hard"]
+                    },
+                    "Type": {
+                        "type": "string",
+                        "description": "Type of the question",
+                        "enum": ["专业基础知识", "计算", "逻辑推理"]
+                    },
+                    "Domain": {
+                        "type": "string",
+                        "description": "Domain of the question",
+                        "enum": ["环境工程学", "大气环境学", "水环境学"]
+                    }
+                },
+                "required": ["Question", "Answer", "Category"]
+            },
+            "minItems": 1,
+            "maxItems": 1
+        }
+    },
+    "required": ["qa_pairs"]
+}
+structured_llm = llm.with_structured_output(json_schema)
+
+# 遍历文件夹中的pickle文件
+pickle_filename = "0aba53cf-4abf-4279-b3b5-75b5d704440f.pkl"
+with open(pickle_filename, "rb") as f:
+    text_list = pickle.load(f)
+
+data = merge_pickle_list(text_list)
+data = fix_utf8(data)
+# data = trim_data(data)
+
+qa_list = []
+
+for i in range(0, len(data), 20):
+    chunk = data[i : i + 20]
+    if len(chunk) < 10:
+        break
+
+    # 将chunk转换为字符串
+    chunk_text = " ".join([text[0] for text in chunk])
+
+    # 执行structured_llm，生成问题和答案
+    response = structured_llm.invoke(
+        f"""基于以下内容，编写1个问答对，用来测试大语言模型及其相关应用（比如rag）在生态环境专业领域中的能力和表现: 
+
+        {chunk_text}
+
+        问题要尽量难一点，并提供相应的详细答案（需要详细的阐述）。"""
+    )
+
+    print(response)
+    qa_pair = response.get('qa_pairs', [])
+
+     # 添加到qa_list
+    qa_list.append({
+        'Question': qa_pair['Question'],
+        'Answer': qa_pair['Answer'],
+        'Level': qa_pair['Level'],
+        'Type': qa_pair['Type'],
+        'Domain': qa_pair['Domain'],
+        'PickleFile': pickle_filename
+    })
+
+# 将qa_list转换为DataFrame并保存到csv文件
+df = pd.DataFrame(qa_list)
+df.to_csv("output.csv", index=False, encoding='utf-8')
---
+++ tools/data.py
@@ -0,0 +1,104 @@
+import logging
+import pickle
+from io import StringIO
+import pandas as pd
+
+import tiktoken
+from bs4 import BeautifulSoup
+from dotenv import load_dotenv
+from tenacity import retry, stop_after_attempt, wait_fixed
+
+
+def num_tokens_from_string(string: str) -> int:
+    """Returns the number of tokens in a text string."""
+    encoding = tiktoken.get_encoding("cl100k_base")
+    return len(encoding.encode(string))
+
+
+def fix_utf8(original_list):
+    cleaned_list = []
+    for original_str in original_list:
+        cleaned_str = original_str[0].replace("\ufffd", " ")
+        cleaned_list.append([cleaned_str, original_str[1]])
+    return cleaned_list
+
+
+def load_pickle_list(file_path):
+    with open(file_path, "rb") as f:
+        data = pickle.load(f)
+    # clean_data = [item[0] for item in data if isinstance(item, tuple)]
+
+    return data
+
+
+def split_dataframe_table(html_table, chunk_size=8100):
+    dfs = pd.read_html(StringIO(html_table))
+    if not dfs:
+        return []
+
+    df = dfs[0]
+    tables = []
+    sub_df = pd.DataFrame()
+    token_count = 0
+
+    for _, row in df.iterrows():
+        row_html = row.to_frame().T.to_html(index=False, border=0, classes=None)
+        row_token_count = num_tokens_from_string(row_html)
+
+        if token_count + row_token_count > chunk_size and not sub_df.empty:
+            sub_html = sub_df.to_html(index=False, border=0, classes=None)
+            tables.append(sub_html)
+            sub_df = pd.DataFrame()
+            token_count = 0
+
+        sub_df = pd.concat([sub_df, row.to_frame().T])
+        token_count += row_token_count
+
+    if not sub_df.empty:
+        sub_html = sub_df.to_html(index=False, border=0, classes=None)
+        tables.append(sub_html)
+
+    return tables
+
+
+def merge_pickle_list(data):
+    temp = ""
+    result = []
+    for d in data:
+        if num_tokens_from_string(d[0]) > 8100:
+            soup = BeautifulSoup(d[0], "html.parser")
+            tables = soup.find_all("table")
+            for table in tables:
+                table_content = str(table)
+                if num_tokens_from_string(table_content) < 8100:
+                    if table_content:  # check if table_content is not empty
+                        result.append([table_content, d[1]])
+                else:
+                    try:
+                        sub_tables = split_dataframe_table(table_content)
+                        for sub_table in sub_tables:
+                            if sub_table:
+                                soup = BeautifulSoup(sub_table, "html.parser")
+                                result.append([str(soup), d[1]])
+                    except Exception as e:
+                        logging.error(f"Error splitting dataframe table: {e}")
+        elif num_tokens_from_string(d[0]) < 15:
+            temp += d[0] + " "
+        else:
+            result.append([(temp + d[0]), d[1]])
+            temp = ""
+    if temp:
+        result.append([temp, d[1]])
+
+    return result
+
+def trim_data(data):
+    """
+    去掉列表前后10%的元素
+
+    :param data: 包含文本的列表
+    :return: 去掉前后10%元素后的列表
+    """
+    n = len(data)
+    remove_count = int(n * 0.05)
+    return data[remove_count:n-remove_count]
